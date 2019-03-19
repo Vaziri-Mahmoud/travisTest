c4f0965639be90906bcd301a38c4f01f77ad6d0a
Mahmoud vaziri <vaziri_mahmoud@yahoo.com>
2019-03-19 22:37:52 +0330

    test

---
+++ code.py
@@ -0,0 +1,9 @@
+print("Hi\nTravis CI test lang:python ")
+print("Happy new year : ", 1398, " :) ")
+print("\n")
+
+
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+arr2 = np.power(arr, 2)
+print(arr2)
