534ce017787b4ebdb014e33c3d01ba581606ea2e
Mahmoud vaziri <vaziri_mahmoud@yahoo.com>
2019-03-21 11:19:14 +0330

    code.py now has class

--- code.py
+++ code.py
@@ -1,9 +1,26 @@
-print("Hi\nTravis CI test lang:python ")
-print("Happy new year : ", 1398, " :) ")
-print("\n")
 
 
-import numpy as np
-arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-arr2 = np.power(arr, 2)
-print(arr2)
+class mahmoud98():
+    def pr():
+        print("Hi\nTravis CI test lang:python ")
+        print("Happy new year : ", 1398, " :) ")
+        print("\n")
+
+    def nump():
+
+        import numpy as np
+        arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        arr2 = np.power(arr, 2)
+        print(arr2)
+
+    def op(a, b):
+        if a == b:
+            print('a = b')
+        else:
+            raise ValueError("Error a != b")
+
+
+if __name__ == '__main__':
+    mahmoud98.pr()
+    # nump()
+    mahmoud98.op(5, 3)
