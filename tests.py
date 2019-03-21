953ea6a3c64dd781b8368a2fd945868630b5bacb
Mahmoud vaziri <vaziri_mahmoud@yahoo.com>
2019-03-21 22:03:15 +0330

    add equal test to tests.py

--- tests.py
+++ tests.py
@@ -8,6 +8,9 @@ class mahmoud98(unittest.TestCase):
         with self.assertRaises(ValueError):
             code.mahmoud98.op(3, 8)
 
+    def test_good_val(self):
+        assert('a = b')
+
 
 if __name__ == '__main__':
     unittest.main()
