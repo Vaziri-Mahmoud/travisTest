651eb70c2eb8901b301ae4ddbb52a071e8b5eca6
Mahmoud vaziri <vaziri_mahmoud@yahoo.com>
2019-03-21 23:27:34 +0330

    comments two line for checking coverage precent

--- tests.py
+++ tests.py
@@ -7,9 +7,10 @@ class mahmoud98(unittest.TestCase):
     def test_bad_val(self):
         with self.assertRaises(ValueError):
             code.mahmoud98.op(3, 8)
-
+    '''
     def test_good_val(self):
         self.assertEqual(code.mahmoud98.op(3, 3), 'a = b')
+    '''
 
 
 if __name__ == '__main__':
