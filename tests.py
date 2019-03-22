13cf1e6c5bccd31c503f528b14df5af69937b2d1
Mahmoud vaziri <vaziri_mahmoud@yahoo.com>
2019-03-22 10:33:25 +0430

    add class  to tests.py

--- tests.py
+++ tests.py
@@ -1,12 +1,12 @@
 import unittest
 
-import code
+from code import mahmoud98
 
 
 class testclass(unittest.TestCase):
     def test_bad_val(self):
         with self.assertRaises(ValueError):
-            code.mahmoud98.op(3, 8)
+            mahmoud98.op(3, 8)
     '''
     def test_good_val(self):
         self.assertEqual(code.mahmoud98.op(3, 3), 'a = b')
