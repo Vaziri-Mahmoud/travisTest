f7afeecd5337541d0943a995330d16df3624af76
Mahmoud vaziri <vaziri_mahmoud@yahoo.com>
2019-03-22 10:51:43 +0430

    add more tests

--- tests.py
+++ tests.py
@@ -7,10 +7,12 @@ class testclass(unittest.TestCase):
     def test_bad_val(self):
         with self.assertRaises(ValueError):
             mahmoud98.op(3, 8)
-    '''
+
     def test_good_val(self):
-        self.assertEqual(code.mahmoud98.op(3, 3), 'a = b')
-    '''
+        self.assertEqual(mahmoud98.op(3, 3), 'a = b')
+
+    def test_op2(self):
+        self.assertEqual(mahmoud98.op2(5), 10)
 
 
 if __name__ == '__main__':
